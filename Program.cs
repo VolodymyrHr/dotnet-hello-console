5597a9507c7b81196b02ff1c9f1c0beb68d65b7a
VolodymyrHr <volodymyrhr@tahometer.com>
2020-08-04 12:21:02 +0300

    create function CounWorkingDays

--- Program.cs
+++ Program.cs
@@ -4,15 +4,33 @@ namespace hello_console
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            String message = BuildMessage("Friend");
-            Console.WriteLine(message);
+            DateTime start = new DateTime(2020, 7, 1);
+            DateTime end = new DateTime(2020, 3, 30);
+            DateTime[] holiday = {new DateTime(2020, 3, 12), new DateTime(2020, 3, 20)};
+
+            Console.WriteLine(CountWorkingDays(start, end, holiday));
+        }
+        public static int CountWorkingDays(DateTime start, DateTime end, DateTime[] holiday)
+        {
+            int countday = 0;
+
+            for (var dt = start; dt <= end; dt = dt.AddDays(1))
+            {
+                // for(var i=0; i<holiday.Length; i++){
+                //     Console.WriteLine(dt);
+                //     if(dt == holiday[i]){
+                //         countday++;
+                //     }
+                // }
+                if(!Array.Exists(holiday, element => element.Equals(dt))){
+                    countday++;
+                }
+            }
+
+            return countday;
         }
 
-        static String BuildMessage(String Name)
-        {
-            return ("Hello " + Name + "!");
-        }
     }
 }
